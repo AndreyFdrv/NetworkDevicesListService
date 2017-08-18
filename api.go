0111220cd2330d19f587a0c0d3c4ca68331a6aa2
AndreyFedorov <andrei_hedorov@mail.ru>
2017-08-18 12:05:47 -0700

    Вывод списка сетевых устройств

--- api.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/codegangsta/martini"
-)
-func GetApi() *martini.ClassicMartini {
-	api := martini.Classic()
-	return api
-}
--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
+
+func writeNetworkDevicesList(w http.ResponseWriter, r *http.Request) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	fmt.Fprintf(w, "List of network devices:\n\n")
+	for i := 0; i < len(interfaces); i++ {
+		fmt.Fprintf(w, "Name: %v; ", interfaces[i].Name)
+		var address string
+		if fmt.Sprintf("%v", interfaces[i].HardwareAddr) == "" {
+			address = "-"
+		} else {
+			address = fmt.Sprintf("%v", interfaces[i].HardwareAddr)
+		}
+		fmt.Fprintf(w, "Address: %v; ", address)
+		fmt.Fprintf(w, "Index: %v; ", interfaces[i].Index)
+		fmt.Fprintf(w, "Maximum transmission unit: %v bytes; ", interfaces[i].MTU)
+		fmt.Fprintf(w, "Flags: %v\n", interfaces[i].Flags)
+	}
+}
 func main() {
-	api:=GetApi()
-	api.Run()
+	fmt.Println("Listening on port :3000")
+	http.HandleFunc("/", writeNetworkDevicesList)
+	http.ListenAndServe(":3000", nil)
 }
